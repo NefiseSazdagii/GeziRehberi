57a35422bf7175da84ca74fb52b92536616d3d63
Nefise Sazdagi <Nefise.sazdagii@gmail.com>
2019-05-28 15:01:47 +0300

    +

---
+++ scotland/scotland/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace scotland.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Istanbul()
+        {
+            return View();
+        }
+        public ActionResult Nevsehir()
+        {
+            return View();
+        }
+        public ActionResult Trabzon()
+        {
+            return View();
+        }
+        public ActionResult Antalya()
+        {
+            return View();
+        }
+        public ActionResult Izmır()
+        {
+            return View();
+        }
+        public ActionResult Balıkesir()
+        {
+            return View();
+        }
+        public ActionResult Mardin()
+        {
+            return View();
+        }
+  
+        public ActionResult Canakkale()
+        {
+            return View();
+        }
+        public ActionResult Ankara()
+        {
+            return View();
+        }
+    }
+}
---
+++ scotland/scotland/assets/js/myScripts.js
@@ -0,0 +1,256 @@
+﻿/// <reference path="jquery-3.2.1.min_2ef39560.js" />
+
+$(document).click(function () {
+    if ($("#aciklamaCamlica").css("visibility") == "visible") {
+        $("#aciklamaCamlica").css("visibility", "hidden");
+        $("#divCamlica").css("opacity", "1");
+    }
+    else if ($("#aciklamaOtagtepe").css("visibility") == "visible") {
+        $("#aciklamaOtagtepe").css("visibility", "hidden");
+        $("#divOtagtepe").css("opacity", "1");
+    }
+    else if ($("#aciklamaKizKulesi").css("visibility") == "visible") {
+        $("#aciklamaKizKulesi").css("visibility", "hidden");
+        $("#divKizKulesi").css("opacity", "1");
+    }
+    else if ($("#aciklamaCamlicaTepe").css("visibility") == "visible") {
+        $("#aciklamaCamlicaTepe").css("visibility", "hidden");
+        $("#divCamlicaTepe").css("opacity", "1");
+    }
+    else if ($("#aciklamaAtasehir").css("visibility") == "visible") {
+        $("#aciklamaAtasehir").css("visibility", "hidden");
+        $("#divAtasehir").css("opacity", "1");
+    }
+    else if ($("#aciklamaBelgrad").css("visibility") == "visible") {
+        $("#aciklamaBelgrad").css("visibility", "hidden");
+        $("#divBelgrad").css("opacity", "1");
+    }
+    else if ($("#aciklamaIstiklal").css("visibility") == "visible") {
+        $("#aciklamaIstiklal").css("visibility", "hidden");
+        $("#divIstiklal").css("opacity", "1");
+    }
+    else if ($("#aciklamaDolmabahce").css("visibility") == "visible") {
+        $("#aciklamaDolmabahce").css("visibility", "hidden");
+        $("#divDolmabahce").css("opacity", "1");
+    }
+    else if ($("#aciklamaGalataKoprusu").css("visibility") == "visible") {
+        $("#aciklamaGalataKoprusu").css("visibility", "hidden");
+        $("#divGalataKoprusu").css("opacity", "1");
+    }
+    else if ($("#aciklamaPierreLoti").css("visibility") == "visible") {
+        $("#aciklamaPierreLoti").css("visibility", "hidden");
+        $("#divPierreLoti").css("opacity", "1");
+    }
+    else if ($("#aciklamaSultanahmet").css("visibility") == "visible") {
+        $("#aciklamaSultanahmet").css("visibility", "hidden");
+        $("#divSultanahmet").css("opacity", "1");
+    }
+    else if ($("#aciklamaMiniaTürk").css("visibility") == "visible") {
+        $("#aciklamaMiniaTürk").css("visibility", "hidden");
+        $("#divMiniaTürk").css("opacity", "1");
+    }
+    else if ($("#aciklamaTopkapi").css("visibility") == "visible") {
+        $("#aciklamaTopkapi").css("visibility", "hidden");
+        $("#divTopkapi").css("opacity", "1");
+    }
+    else if ($("#aciklamaKapaliCarsi").css("visibility") == "visible") {
+        $("#aciklamaKapaliCarsi").css("visibility", "hidden");
+        $("#divKapaliCarsi").css("opacity", "1");
+    }
+    else if ($("#aciklamaYerebatanSarnici").css("visibility") == "visible") {
+        $("#aciklamaYerebatanSarnici").css("visibility", "hidden");
+        $("#divYerebatanSarnici").css("opacity", "1");
+    }
+    else if ($("#aciklamaRumeliHisari").css("visibility") == "visible") {
+        $("#aciklamaRumeliHisari").css("visibility", "hidden");
+        $("#divRumeliHisari").css("opacity", "1");
+    }
+    else if ($("#aciklamaSafirKule").css("visibility") == "visible") {
+        $("#aciklamaSafirKule").css("visibility", "hidden");
+        $("#divSafirKule").css("opacity", "1");
+    }
+    else if ($("#aciklamaEmirgan").css("visibility") == "visible") {
+        $("#aciklamaEmirgan").css("visibility", "hidden");
+        $("#divEmirgan").css("opacity", "1");
+    }
+    else if ($("#aciklamaAnadoluKavagi").css("visibility") == "visible") {
+        $("#aciklamaAnadoluKavagi").css("visibility", "hidden");
+        $("#divAnadoluKavagi").css("opacity", "1");
+    }
+    else if ($("#aciklamaHidivKasri").css("visibility") == "visible") {
+        $("#aciklamaHidivKasri").css("visibility", "hidden");
+        $("#divHidivKasri").css("opacity", "1");
+    }
+    else if ($("#aciklamaAnadoluHisari").css("visibility") == "visible") {
+        $("#aciklamaAnadoluHisari").css("visibility", "hidden");
+        $("#divAnadoluHisari").css("opacity", "1");
+    }
+    else if ($("#aciklamaKapadokya").css("visibility") == "visible") {
+        $("#aciklamaKapadokya").css("visibility", "hidden");
+        $("#divKapadokya").css("opacity", "1");
+    }
+    else if ($("#aciklamaPeribacalari").css("visibility") == "visible") {
+        $("#aciklamaPeribacalari").css("visibility", "hidden");
+        $("#divPeribacalari").css("opacity", "1");
+    }
+    else if ($("#aciklamaIhlara").css("visibility") == "visible") {
+        $("#aciklamaIhlara").css("visibility", "hidden");
+        $("#divIhlara").css("opacity", "1");
+    }
+    else if ($("#aciklamaGoreme").css("visibility") == "visible") {
+        $("#aciklamaGoreme").css("visibility", "hidden");
+        $("#divGoreme").css("opacity", "1");
+    }
+    else if ($("#aciklamaZelve").css("visibility") == "visible") {
+        $("#aciklamaZelve").css("visibility", "hidden");
+        $("#divZelve").css("opacity", "1");
+    }
+    else if ($("#aciklamaUcHisar").css("visibility") == "visible") {
+        $("#aciklamaUcHisar").css("visibility", "hidden");
+        $("#divUcHisar").css("opacity", "1");
+    }
+    else if ($("#aciklamaUrgup").css("visibility") == "visible") {
+        $("#aciklamaUrgup").css("visibility", "hidden");
+        $("#divUrgup").css("opacity", "1");
+    }
+    else if ($("#aciklamaIbrahım").css("visibility") == "visible") {
+        $("#aciklamaIbrahım").css("visibility", "hidden");
+        $("#divIbrahım").css("opacity", "1");
+    }
+    else if ($("#aciklamaYerAltiSehir").css("visibility") == "visible") {
+        $("#aciklamaYerAltiSehir").css("visibility", "hidden");
+        $("#divYerAltiSehir").css("opacity", "1");
+    }
+    else if ($("#aciklamaSumela").css("visibility") == "visible") {
+        $("#aciklamaSumela").css("visibility", "hidden");
+        $("#divSumela").css("opacity", "1");
+    }
+    else if ($("#aciklamaVazelon").css("visibility") == "visible") {
+        $("#aciklamaVazelon").css("visibility", "hidden");
+        $("#divVazelon").css("opacity", "1");
+    }
+    else if ($("#aciklamaUzungol").css("visibility") == "visible") {
+        $("#aciklamaUzungol").css("visibility", "hidden");
+        $("#divUzungol").css("opacity", "1");
+    }
+    else if ($("#aciklamaPeristera").css("visibility") == "visible") {
+        $("#aciklamaPeristera").css("visibility", "hidden");
+        $("#divPeristera").css("opacity", "1");
+    }
+    else if ($("#aciklamaCalMagarası").css("visibility") == "visible") {
+        $("#aciklamaCalMagarası").css("visibility", "hidden");
+        $("#divCalMagarası").css("opacity", "1");
+    }
+    else if ($("#aciklamaAltındereVadisi").css("visibility") == "visible") {
+        $("#aciklamaAltındereVadisi").css("visibility", "hidden");
+        $("#divAltındereVadisi").css("opacity", "1");
+    }
+    else if ($("#aciklamaAtatürkKösküMüzesi").css("visibility") == "visible") {
+        $("#aciklamaAtatürkKösküMüzesi").css("visibility", "hidden");
+        $("#divAtatürkKösküMüzesi").css("opacity", "1");
+    }
+    else if ($("#aciklamaDuzkoy").css("visibility") == "visible") {
+        $("#aciklamaDuzkoy").css("visibility", "hidden");
+        $("#divDuzkoy").css("opacity", "1");
+    }
+    else if ($("#aciklamaZagnosVadisi").css("visibility") == "visible") {
+        $("#aciklamaZagnosVadisi").css("visibility", "hidden");
+        $("#divZagnosVadisi").css("opacity", "1");
+    }
+    else if ($("#aciklamaAltinbesikMagarasi").css("visibility") == "visible") {
+        $("#aciklamaAltinbesikMagarasi").css("visibility", "hidden");
+        $("#divAltinbesikMagarasi").css("opacity", "1");
+    }
+    else if ($("#aciklamaManavgatSelalesi").css("visibility") == "visible") {
+        $("#aciklamaManavgatSelalesi").css("visibility", "hidden");
+        $("#divManavgatSelalesi").css("opacity", "1");
+    }
+    else if ($("#aciklamaYanartas").css("visibility") == "visible") {
+        $("#aciklamaYanartas").css("visibility", "hidden");
+        $("#divYanartas").css("opacity", "1");
+    }
+    else if ($("#aciklamaOyuncakMuzesi").css("visibility") == "visible") {
+        $("#aciklamaOyuncakMuzesi").css("visibility", "hidden");
+        $("#divOyuncakMuzesi").css("opacity", "1");
+    }
+    else if ($("#aciklamaKursunluSelalesi").css("visibility") == "visible") {
+        $("#aciklamaKursunluSelalesi").css("visibility", "hidden");
+        $("#divKursunluSelalesi").css("opacity", "1");
+    }
+    else if ($("#aciklamaKopruluKanyon").css("visibility") == "visible") {
+        $("#aciklamaKopruluKanyon").css("visibility", "hidden");
+        $("#divKopruluKanyon").css("opacity", "1");
+    }
+    else if ($("#aciklamakonyaalti").css("visibility") == "visible") {
+        $("#aciklamakonyaalti").css("visibility", "hidden");
+        $("#divkonyaalti").css("opacity", "1");
+    }
+
+    else if ($("#aciklamaKayakMerkezi").css("visibility") == "visible") {
+        $("#aciklamaKayakMerkezi").css("visibility", "hidden");
+        $("#divKayakMerkezi").css("opacity", "1");
+    }
+
+    else if ($("#aciklamaDimcayi").css("visibility") == "visible") {
+        $("#aciklamaDimcayi").css("visibility", "hidden");
+        $("#divDimcayi").css("opacity", "1");
+    }
+    else if ($("#aciklamaDikili").css("visibility") == "visible") {
+        $("#aciklamaDikili").css("visibility", "hidden");
+        $("#divDikili").css("opacity", "1");
+    }
+    else if ($("#aciklamaSeferhisar").css("visibility") == "visible") {
+        $("#aciklamaSeferhisar").css("visibility", "hidden");
+        $("#divSeferhisar").css("opacity", "1");
+    }
+    else if ($("#aciklamaEfes").css("visibility") == "visible") {
+        $("#aciklamaEfes").css("visibility", "hidden");
+        $("#divEfes").css("opacity", "1");
+    }
+    else if ($("#aciklamaAlacati").css("visibility") == "visible") {
+        $("#aciklamaAlacati").css("visibility", "hidden");
+        $("#divAlacati").css("opacity", "1");
+    }
+    else if ($("#aciklamaSirince").css("visibility") == "visible") {
+        $("#aciklamaSirince").css("visibility", "hidden");
+        $("#divSirince").css("opacity", "1");
+    }
+    else if ($("#aciklamaTire").css("visibility") == "visible") {
+        $("#aciklamaTire").css("visibility", "hidden");
+        $("#divTire").css("opacity", "1");
+    }
+    else if ($("#aciklamaKaraburun").css("visibility") == "visible") {
+        $("#aciklamaKaraburun").css("visibility", "hidden");
+        $("#divKaraburun").css("opacity", "1");
+    }
+    else if ($("#aciklamaBayindir").css("visibility") == "visible") {
+        $("#aciklamaBayindir").css("visibility", "hidden");
+        $("#divBayindir").css("opacity", "1");
+    }
+    else if ($("#aciklamaBergama").css("visibility") == "visible") {
+        $("#aciklamaBergama").css("visibility", "hidden");
+        $("#divBergama").css("opacity", "1");
+    }
+    else if ($("#aciklamaCamlibel").css("visibility") == "visible") {
+        $("#aciklamaCamlibel").css("visibility", "hidden");
+        $("#divCamlibel").css("opacity", "1");
+    }
+    else if ($("#aciklamaAntandros").css("visibility") == "visible") {
+        $("#aciklamaAntandros").css("visibility", "hidden");
+        $("#divAntandros").css("opacity", "1");
+    }
+    else if ($("#aciklamaYazoren").css("visibility") == "visible") {
+        $("#aciklamaYazoren").css("visibility", "hidden");
+        $("#divYazoren").css("opacity", "1");
+    }
+});
+
+function YaziGoster(divResimID, aciklamaResimID) {
+    $(document).ready(function () {
+        var divResim = $('#' + divResimID);
+        var aciklamaResim = $('#' + aciklamaResimID);
+        aciklamaResim.css("visibility", "visible");
+        divResim.css("opacity", "0.5");
+        aciklamaResim.css("opacity", "1");
+    });
+};
